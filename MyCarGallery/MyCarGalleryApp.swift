3eeccfbaab309593891687a2a39c639b4dffc9bd
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-12-13 14:18:48 +0100

    Initial Commit

---
+++ MyCarGallery/MyCarGalleryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarGalleryApp.swift
+//  MyCarGallery
+//
+//  Created by Bernadette Werneke on 13.12.23.
+//
+
+import SwiftUI
+
+@main
+struct MyCarGalleryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
