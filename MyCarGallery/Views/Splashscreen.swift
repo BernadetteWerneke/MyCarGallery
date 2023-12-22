2c1bf396bb74ce6590bf792838128fa4af77a6d2
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-12-22 21:07:28 +0100

    .1. Erstellung App Icon  2. Splashscreen mit Animation (keyframes)

---
+++ MyCarGallery/Views/Splashscreen.swift
@@ -0,0 +1,18 @@
+//
+//  Splashscreen.swift
+//  MyCarGallery
+//
+//  Created by Bernadette Werneke on 22.12.23.
+//
+
+import SwiftUI
+
+struct Splashscreen: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Splashscreen()
+}
