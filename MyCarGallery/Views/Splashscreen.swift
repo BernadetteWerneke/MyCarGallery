0dcb44a1709f03f03806cb2d41dad608f5fbf113
Bernadette Werneke <bernadettewerneke@gmail.com>
2023-12-22 21:18:39 +0100

    App Icon, Splashscreen

--- MyCarGallery/Views/Splashscreen.swift
+++ MyCarGallery/Views/Splashscreen.swift
@@ -1,18 +1,91 @@
 //
-//  Splashscreen.swift
+//  LaunchScreenView.swift
 //  MyCarGallery
 //
-//  Created by Bernadette Werneke on 22.12.23.
+//  Created by Bernadette Werneke on 18.12.23.
 //
 
 import SwiftUI
 
-struct Splashscreen: View {
+struct AnimationProperties {
+    var translation = 0.0
+    var verticalStretch = 1.0
+}
+
+
+struct LaunchScreenView: View {
+    
+    @State private var selected = false
+    @State private var isActive = false
+    var totalDuration = 1.0
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        if isActive {
+            ContentView()
+        } else {
+            
+            //Splashscreen
+            ZStack {
+                //Hintrgrund mit Farbverlauf
+                LinearGradient(colors: [.white, .black], startPoint: .topLeading, endPoint: .bottomTrailing).ignoresSafeArea()
+                
+                //animiertes Image
+                Image(systemName: "car.circle.fill")
+                    .resizable()
+                    .foregroundStyle(.white)
+                    .frame(width: 200, height: 200)
+                    .keyframeAnimator(initialValue: AnimationProperties(), repeating: true) { content, value in
+                        content
+                            .scaleEffect(.init(width: 1, height: value.verticalStretch), anchor: .bottom)
+                            .offset(y: value.translation)
+                    } keyframes: { _ in
+                        KeyframeTrack(\.verticalStretch) {
+                            SpringKeyframe(0.6, duration: 0.2 * totalDuration)
+                            CubicKeyframe(1, duration: 0.2 * totalDuration)
+                            CubicKeyframe(1.2, duration: 0.6 * totalDuration)
+                            CubicKeyframe(1.1, duration: 0.25 * totalDuration)
+                            SpringKeyframe(1, duration: 0.25 * totalDuration)
+                        }
+                        
+                        KeyframeTrack(\.translation) {
+                            CubicKeyframe(0, duration: 0.2 * totalDuration)
+                            CubicKeyframe(-100, duration: 0.4 * totalDuration)
+                            CubicKeyframe(-100, duration: 0.4 * totalDuration)
+                            CubicKeyframe(0, duration: 0.5 * totalDuration)
+                        }
+                    }
+            }
+            
+            //Dauer der Animation
+            .onAppear {
+                DispatchQueue.main.asyncAfter(deadline: .now() + 2.4) {
+                    //Übergang wird weicher
+                    withAnimation {
+                        self.isActive = true
+                    }
+                  
+                }
+            }
+        }
     }
 }
 
+
 #Preview {
-    Splashscreen()
+    LaunchScreenView()
 }
+
+
+/*struct Triangle: Shape {
+    func path(in rect: CGRect) -> Path {
+        var path = Path()
+        
+        path.move(to: CGPoint(x: rect.midX, y: rect.minY))
+        path.addLine(to: CGPoint(x: rect.minX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.maxX, y: rect.maxY))
+        path.addLine(to: CGPoint(x: rect.midX, y: rect.minY))
+        
+        return path
+    }
+}*/
